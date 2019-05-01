d41a45f0efea35bf298b0b62fcf54e1bd1b3396f
Ida Hallner <bee_ida@hotmail.com>
2019-05-01 18:07:02 +0200

    add acf and try one post

--- wordplate/public/themes/spacecrafts/page-templates/spacecraft.php
+++ wordplate/public/themes/spacecrafts/page-templates/spacecraft.php
@@ -1,19 +1,38 @@
+<?php /* Template Name: Spacecrafts */ ?>
+
 <?php get_header(); ?>
 
 <div class="row">
     <div class="col">
+        <?php echo ('hello'); ?>
         <?php if (have_posts()) : ?>
 
             <?php while (have_posts()): the_post(); ?>
 
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <h1><?php the_title(); ?></h1>
 
-                <?php the_excerpt(); ?>
+                <?php the_content(); ?>
 
             <?php endwhile; ?>
-
         <?php endif; ?>
-    </div><!-- /col -->
+</div><!-- /col -->
 </div><!-- /row -->
+<?php
+$spacecrafts = get_posts(array('post_type' => 'spacecraft'));
+?>
+
+<div class="row">
+  <ul>
+    <?php foreach ($spacecrafts as $spacecraft): ?>
+                <div class="col mt-4">
+                    <h3><a href="<?php the_permalink($spacecraft); ?>"><?php echo $spacecraft->post_title; ?></a></h3>
+                    <?php the_excerpt(); ?>
+                    <img src="<?php echo the_field('image', $spacecraft); ?>" />
+                    <p>Event Date: <?php echo the_field('name', $spacecraft); ?></p>
+                    <p>Event Date: <?php echo the_field('content', $spacecraft); ?></p>
+
+    <?php endforeach; ?>
+  </ul>
+
 
 <?php get_footer(); ?>
